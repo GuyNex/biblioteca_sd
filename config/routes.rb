ff142bce15eebbfe873f66dd881d48fa77408726
GuyNex <macorreia123456@gmail.com>
2021-03-19 20:15:09 -0300

    crud dos livros e parte do frontend :lipstick: :alien:

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'adicionar', to: 'books#new'
+  get 'mostrar', to: 'books#index'
   root 'application#homepage'
+  mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
+  resources :books
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
